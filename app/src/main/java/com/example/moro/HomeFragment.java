a88de2c1feffdf3c6dcf30fadbd5c0e81e5e9c40
xDelux <s195467@student.dtu.dk>
2020-10-28 21:26:35 +0100

    ListView nu synligt i fragment_home.xml

--- app/src/main/java/com/example/moro/HomeFragment.java
+++ app/src/main/java/com/example/moro/HomeFragment.java
@@ -4,15 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
+
 public class HomeFragment extends Fragment {
+
+    String[] testEvents = {"Softball", "Kunstner fremvisning", "Natklub åbning", "Vin smagning", "Kulturnat", "Øl smagning", "Pudekamp på rådhuspladsen", "Nøgenløb", "Ild festival"};
+    ListView eventList;
+    ArrayAdapter<String> eventAdapter;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+        eventList = (ListView) view.findViewById(R.id.eventlistview);
+        eventAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, testEvents);
+        eventList.setAdapter(eventAdapter);
+
+        return view;
     }
 }
