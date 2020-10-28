64781522f32ee6a4048dd210d98941089ff5fb00
xDelux <s195467@student.dtu.dk>
2020-10-28 18:57:07 +0100

    Added fragment support in MainActivity.java (Will probably changed in the future, since it's very inefficient code.)

--- app/src/main/java/com/example/moro/MainActivity.java
+++ app/src/main/java/com/example/moro/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.moro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemReselectedListener(navListener);
     }
+    private BottomNavigationView.OnNavigationItemReselectedListener navListener =
+            new BottomNavigationView.OnNavigationItemReselectedListener() {
+        @Override
+        public void onNavigationItemReselected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.bot_nav_home :
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.bot_nav_events :
+                    //selectedFragment = new HomeFragment();
+                    break;
+                case R.id.bot_nav_favorite :
+                    //selectedFragment = new HomeFragment();
+                    break;
+                case R.id.bot_nav_menu :
+                    //selectedFragment = new HomeFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment_container, selectedFragment).commit();
+
+        }
+    };
+
 }
