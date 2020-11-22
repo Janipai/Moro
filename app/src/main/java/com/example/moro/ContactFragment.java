46a79d604fbf8e2baaeee91504e4d560e2462afb
Mjohansen1997 <broendby97>
2020-11-22 13:30:58 +0100

    implemented the buttons in Contacts

--- app/src/main/java/com/example/moro/ContactFragment.java
+++ app/src/main/java/com/example/moro/ContactFragment.java
@@ -13,13 +13,13 @@ public class ContactFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_contact, container,false);
 
         Button tipButton = (Button) view.findViewById(R.id.tipButton);
         tipButton.setOnClickListener(this);
 
-        // Inflate the layout for this fragment
+
         return view;
     }
 
