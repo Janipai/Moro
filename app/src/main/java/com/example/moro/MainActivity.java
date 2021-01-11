4c5ed9eb2fd0ae200c60da96332285fa411eb21f
Janipai <s195477@student.dtu.dk>
2021-01-11 12:11:30 +0100

    commit

--- app/src/main/java/com/example/moro/MainActivity.java
+++ app/src/main/java/com/example/moro/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Sentry.captureMessage("testing SDK setup");
+
         // SENTRY TEST
         try {
             throw new Exception("This is a test of sentry.");
