b49b8f7a1d8f761bcf959760ac34053a85b5d225
Jacob <s174130@student.dtu.dk>
2021-01-11 16:05:51 +0100

    Made the DTOs

--- app/src/main/java/com/example/moro/Data/DTO/EventDTO.java
+++ app/src/main/java/com/example/moro/Data/DTO/EventDTO.java
@@ -1,4 +1,70 @@
 package com.example.moro.Data.DTO;
 
 public class EventDTO {
+
+    private String eventName;
+    private String eventGenre;
+    private String eventDate;
+    private String eventPrice;
+    private String eventInfo;
+    private int eventImage;
+
+    public EventDTO(String eventName, String eventGenre, String eventDate, String eventPrice, String eventInfo, int eventImage){
+        this.eventName = eventName;
+        this.eventGenre = eventGenre;
+        this.eventDate = eventDate;
+        this.eventPrice = eventPrice;
+        this.eventInfo = eventInfo;
+        this.eventImage = eventImage;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventGenre() {
+        return eventGenre;
+    }
+
+    public void setEventGenre(String eventGenre) {
+        this.eventGenre = eventGenre;
+    }
+
+    public String getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(String eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public String getEventPrice() {
+        return eventPrice;
+    }
+
+    public void setEventPrice(String eventPrice) {
+        this.eventPrice = eventPrice;
+    }
+
+    public String getEventInfo() {
+        return eventInfo;
+    }
+
+    public void setEventInfo(String eventInfo) {
+        this.eventInfo = eventInfo;
+    }
+
+    public int getEventImage() {
+        return eventImage;
+    }
+
+    public void setEventImage(int eventImage) {
+        this.eventImage = eventImage;
+    }
+
+
 }
--- app/src/main/java/com/example/moro/Data/DTO/ProfilDTO.java
+++ app/src/main/java/com/example/moro/Data/DTO/ProfilDTO.java
@@ -1,4 +1,59 @@
 package com.example.moro.Data.DTO;
 
 public class ProfilDTO {
+
+    private String profileName;
+    private String profileBirthday;
+    private String profileGender;
+    private String profileEmail;
+    private String profilePassword;
+
+    public ProfilDTO(String profileName, String profileBirthday, String profileGender, String profileEmail, String profilePassword) {
+        this.profileName = profileName;
+        this.profileBirthday = profileBirthday;
+        this.profileGender = profileGender;
+        this.profileEmail = profileEmail;
+        this.profilePassword = profilePassword;
+    }
+
+    public String getProfileName() {
+        return profileName;
+    }
+
+    public void setProfileName(String profileName) {
+        this.profileName = profileName;
+    }
+
+    public String getProfileBirthday() {
+        return profileBirthday;
+    }
+
+    public void setProfileBirthday(String profileBirthday) {
+        this.profileBirthday = profileBirthday;
+    }
+
+    public String getProfileGender() {
+        return profileGender;
+    }
+
+    public void setProfileGender(String profileGender) {
+        this.profileGender = profileGender;
+    }
+
+    public String getProfileEmail() {
+        return profileEmail;
+    }
+
+    public void setProfileEmail(String profileEmail) {
+        this.profileEmail = profileEmail;
+    }
+
+    public String getProfilePassword() {
+        return profilePassword;
+    }
+
+    public void setProfilePassword(String profilePassword) {
+        this.profilePassword = profilePassword;
+    }
+
 }
