0adf67aa563de09641f1d15528ab0c01565fafbc
lillebrian <s195456@student.dtu.dk>
2021-01-14 13:09:25 +0100

    DAO DTO Progress.

--- app/src/main/java/com/example/moro/Data/DAO/ProfileDAO.java
+++ app/src/main/java/com/example/moro/Data/DAO/ProfileDAO.java
@@ -1,4 +1,25 @@
 package com.example.moro.Data.DAO;
 
+import com.example.moro.Data.ADatabaseCon.Connection;
+
+import java.net.UnknownHostException;
+
 public class ProfileDAO {
+    Connection con = Connection.getInstance();
+
+
+    public ProfileDAO () throws UnknownHostException {
+    }
+
+
+
+    public void getUser () {
+
+
+    }
+
+
+
+
+
 }
--- app/src/main/java/com/example/moro/Data/DTO/ProfileDTO.java
+++ app/src/main/java/com/example/moro/Data/DTO/ProfileDTO.java
@@ -11,15 +11,43 @@ public class ProfileDTO {
     ArrayList<EventDTO> favourites;
 
 
-
-    public ProfileDTO() {
-
+    public ProfileDTO (String username, String password, String dateborn, ArrayList<EventDTO> favourites) {
+        this.username = username;
+        this.password = password;
+        this.dateborn = dateborn;
+        this.favourites = favourites;
     }
 
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
+    public String getPassword() {
+        return password;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
+    public String getDateborn() {
+        return dateborn;
+    }
 
+    public void setDateborn(String dateborn) {
+        this.dateborn = dateborn;
+    }
+
+    public ArrayList<EventDTO> getFavourites() {
+        return favourites;
+    }
+
+    public void setFavourites(ArrayList<EventDTO> favourites) {
+        this.favourites = favourites;
+    }
 }
