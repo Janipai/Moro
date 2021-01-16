c34c181b57a84907669cdc4365e9b21286559cb2
Jacob <s174130@student.dtu.dk>
2021-01-16 17:11:04 +0100

    Added some methods to insert, read from collections

--- app/src/main/java/com/example/moro/Data/ADatabaseCon/Connection.java
+++ app/src/main/java/com/example/moro/Data/ADatabaseCon/Connection.java
@@ -1,28 +1,84 @@
 package com.example.moro.Data.ADatabaseCon;
 
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.example.moro.Data.DTO.EventDTO;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
-import com.mongodb.util.JSON;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-import org.bson.BSONObject;
-
-import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.List;
 
 public class Connection {
+    private ArrayList<EventDTO> events = new ArrayList<>();
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-    public void Write(){
-        db.collection("events");
+    public void insertInCollection(String collectionPath, HashMap<String, Object> data){
+        db.collection(collectionPath)
+        .add(data)
+        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Log.d("Tag", "Success transfering data!");
+            }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("Tag", "Something went wrong under transmission");
+            }
+        });
+    }
+
+    public void readFromCollection(String collectionPath){
+        db.collection(collectionPath)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for(QueryDocumentSnapshot document : task.getResult()){
+                                Log.d("Tag", document.getId() + "=> " + document.getData());
+                            }
+                        } else {
+                            Log.w("Tag", "Error getting documents", task.getException());
+                        }
+                    }
+                });
+    }
+
+    public void getAllFromCollection(String collectionPath){
+        db.collection(collectionPath)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        if(queryDocumentSnapshots.isEmpty()){
+                            Log.d("TAG", "OnSuccess the Document is empty");
+                            return;
+                        } else {
+                                List<EventDTO> types = queryDocumentSnapshots.toObjects(EventDTO.class);
+
+                                events.addAll(types);
+                                Log.d("Tag", "OnSuccess" + events);
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w("Tag", "Something went wrong");
+            }
+        });
     }
 }
