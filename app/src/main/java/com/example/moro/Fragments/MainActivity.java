efce8d91114d7eb247d523dec0563af463c9858a
lillebrian <s195456@student.dtu.dk>
2021-01-17 16:53:19 +0100

    merge

--- app/src/main/java/com/example/moro/Fragments/MainActivity.java
+++ app/src/main/java/com/example/moro/Fragments/MainActivity.java
@@ -78,6 +78,7 @@ public class MainActivity extends AppCompatActivity{
 //        getSupportActionBar().setDisplayShowCustomEnabled(true);
 //        getSupportActionBar().setCustomView(R.layout.toptoolbar);
 
+        
 
             bottomNav.setOnNavigationItemSelectedListener(item -> {
                 Fragment selectedFragment = null;
